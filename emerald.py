6982474495cae98203332d03a5e37444a49fcde7
Роман Зельдинов <dexpaz16@gmail.com>
2024-01-30 18:37:17 +0300

    First emerald commit!

---
+++ emerald.py
@@ -0,0 +1,51 @@
+import mushroomlib
+import os
+import shutil
+import sys
+
+working_directory = os.getcwd()
+isRunning = True
+
+def ls():
+    global working_directory
+    listdir = os.listdir(working_directory)
+    for i in listdir:
+        print(i)
+
+def cd():
+    global working_directory
+    pathdir = input("Path: ")
+    fullpath = working_directory + "/" + pathdir
+    isdir = os.path.isdir(fullpath)
+    if (isdir == True):
+        working_directory = fullpath
+    else:
+        print("Directory not found")
+
+def cd_parentdir():
+    global working_directory
+    working_directory = os.path.abspath(os.path.join(working_directory, os.pardir))
+
+def pwd():
+    global working_directory
+    print(working_directory)
+
+commands = {
+"ls":ls,
+"cd":cd,
+"cd ..":cd_parentdir,
+"pwd":pwd,
+"m":mushroomlib.loadmod,
+"loadmod":mushroomlib.loadmod,
+}
+
+
+def console():
+    global working_directory
+    global isRunning
+    while isRunning:
+        command = input(working_directory+" $ ")
+        if command in commands:
+            commands[command]()
+        else:
+            print("Command not found.")
---
+++ mushroomlib.py
@@ -0,0 +1,51 @@
+import os
+import shutil
+import sys
+
+working_directory = ""
+# =========================
+# Disabled, not finished.
+# =========================
+# try:
+#    with open("mushroom_dir.conf", 'r') as f:
+#        working_directory = f.read()
+#except IOError:
+#    pass
+#
+#if (working_directory != ""):
+#    pass
+#elif (working_directory == "" or working_directory == " "):
+#    pass
+# =========================
+
+
+def cf():
+    global working_directory
+    fp = working_directory + "/" + input("Path: ")
+    with open(fp, 'w') as f:
+        f.write("")
+
+def vf():
+    global working_directory
+    fp = working_directory + "/" + input("Path: ")
+    file = open(fp, mode='r', encoding='utf-8')
+    print(file.read())
+
+def wf():
+    global working_directory
+    fp = working_directory + "/" + input("Path: ")
+    data = input("Data: ")
+    with open(fp, 'w') as f:
+        f.write(data)
+
+def loadmod():
+    global working_directory
+    modpath = input("Please, enter mod path: ")
+    with open(modpath) as f2:
+        exec(f2.read())
+
+def mkdir():
+    global working_directory
+    path = working_directory + "/" + input("Path: ")
+    if not os.path.exists(path):
+        os.mkdir(path)
